7b97cca6e59092416d6075f822235b45e66bf6b1
jeelpatel231 <jeelpatel231@gmail.com>
2022-08-12 02:19:05 +0530

    add popular videos to homepage

---
+++ src/components/SingleHomeVideo.svelte
@@ -0,0 +1,62 @@
+<div class="videos">
+    <a href={`/watch?v=${data.videoId}`}>
+        <div class="image-container">
+            <img src={`/proxy/images/${data.videoId}`} alt={data.title}>
+            <div class="duration text-caption">
+                {durationConverter(data.lengthSeconds)}
+            </div>
+        </div>
+        <div class="text-body2 title">
+            {data.title}
+        </div>
+        </a>
+        <a href={data.authorUrl}>
+            <!-- INVIDIOUS DOESNT PROVIDE CHANNEL THUMBNAIL -->
+            <div class="text-caption info">
+                {data.author}
+            </div>
+        </a>
+    <a href={`/watch?v=${data.videoId}`}>
+        <div class="text-caption info">
+            {nFormatter(data.viewCount)} • {data.publishedText}
+        </div>
+    </a>
+</div>
+
+<style>
+.videos{
+    /* min-content sets the width to width of image */
+    width: min-content;
+    margin: 0 8px;
+    margin-top: 8px;
+}
+.image-container{
+    position: relative;
+    width: fit-content;
+}
+.title{
+    margin: 12px 0 6px 0;
+    font-weight: 500;
+}
+.duration{
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 4px;
+    margin: 8px;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: #fff;
+    border-radius: 2px;
+}
+.info{
+    color: #606060;
+    line-height: 1.8rem;
+}
+</style>
+
+<script lang="ts">
+import { durationConverter, nFormatter } from "../store/utils/functions";
+import type Popular from "../interfaces/Popular"
+
+export let data:Popular;
+</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,3 +2,38 @@
     <title>NotYoutube | Home</title>
 </svelte:head>
 <!-- FILL WITH TRENDING/POPULAR -->
+
+<div class="video-container">
+    {#each data as entry}
+        <SingleHomeVideo data={entry}/>
+    {/each}
+</div>
+
+<style>
+.video-container{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+}
+</style>
+
+<script context="module">
+export const load = async ({ fetch }) => {
+    const res = await fetch(`${get(FINAL_HOST)}/api/v1/popular`);
+    const data = await res.json();
+    return{
+        props:{
+            data,
+        }
+    }
+}
+</script>
+
+<script lang="ts">
+import { FINAL_HOST } from "../store/store";
+import { get } from "svelte/store";
+import type Popular from "../interfaces/Popular";
+import SingleHomeVideo from "../components/SingleHomeVideo.svelte";
+
+export let data:Popular[];
+</script>
