d4bf03c75b318f155325186f12c5845160928fea
jeelpatel231 <jeelpatel231@gmail.com>
2022-08-12 01:02:57 +0530

    fetch multiple variants of roboto

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap">
 </svelte:head>
 
 <Navbar/>
